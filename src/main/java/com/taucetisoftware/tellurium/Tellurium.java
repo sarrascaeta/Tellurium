39717f1fcf3411edf9e04c1e1dc4aff87a329dfc
Sergio Arrascaeta <sergio7@gmail.com>
2016-06-19 22:46:56 -0600

    Add new webdriver action methods and corresponding junit tests

--- src/main/java/com/taucetisoftware/tellurium/Tellurium.java
+++ src/main/java/com/taucetisoftware/tellurium/Tellurium.java
@@ -2,22 +2,74 @@ package com.taucetisoftware.tellurium;
 
 import com.taucetisoftware.tellurium.utility.Util;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Sergio on 6/2/2016.
  */
 public class Tellurium extends Selector {
+    private int globalWaitTime = 30;
 
-    private WebDriver driver;
+    protected WebDriver driver;
 
     public Tellurium() {
-        //do nothing
+        //TODO implicit wait is not normally being set
     }
 
     public Tellurium(WebDriver driver) {
         this.driver = driver;
+        setImplicitWait();
+    }
+
+    private void setImplicitWait() {
+        driver.manage().timeouts().implicitlyWait(globalWaitTime, TimeUnit.SECONDS);
+    }
+
+    public void setGlobalWaitTime(int globalWaitInSeconds) {
+        globalWaitTime = globalWaitInSeconds;
+        setImplicitWait();
+    }
+
+    public void go(String url) {
+        driver.get(url);
+    }
+
+    public String getUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    public String getTitle() {
+        return driver.getTitle();
+    }
+
+    public String getPageSource() {
+        return driver.getPageSource();
+    }
+
+    public void goBack() {
+        driver.navigate().back();
+    }
+
+    public void goForward() {
+        driver.navigate().forward();
+    }
+
+    public void refresh() {
+        driver.navigate().refresh();
+    }
+
+    public void executeJavascript(String jsCode) {
+        if (driver instanceof JavascriptExecutor) {
+            ((JavascriptExecutor)driver).executeScript(jsCode);
+        } else {
+            throw new IllegalStateException("This driver does not support JavaScript!");
+        }
     }
 
     public void click(By elementToClick) {
@@ -25,8 +77,22 @@ public class Tellurium extends Selector {
         webElement.click();
     }
 
+    public void sendTo(By elementToSendTo, boolean isClearing, CharSequence... charSequence) {
+        WebElement webElement = createWebElement(elementToSendTo);
+
+        if (isClearing)
+            webElement.clear();
+
+        webElement.sendKeys(charSequence);
+    }
+
+    public void sendTo(By elementToSendTo, CharSequence... charSequence) {
+        sendTo(elementToSendTo, true, charSequence);
+    }
+
+
     private WebElement createWebElement(By elementToClick) {
-        //TODO explicit wait?
+        waitFor(elementToClick);
 
         if (driver == null) {
             throw new RuntimeException("WebDriver is null! Create a webdriver and assign it with: setDriver()");
@@ -35,12 +101,16 @@ public class Tellurium extends Selector {
         return driver.findElement(elementToClick);
     }
 
+    public void waitFor(By elementToWaitFor) {
+        WebDriverWait wait = new WebDriverWait(driver, globalWaitTime);
+        wait.until(ExpectedConditions.elementToBeClickable(elementToWaitFor));
+    }
 
     public void setDriver(WebDriver driver) {
         this.driver = driver;
     }
 
     public void sleep(int sleepTime) {
-	Util.sleep(sleepTime);
+	    Util.sleep(sleepTime);
     }
 }
--- src/test/java/TelluriumTests.java
+++ src/test/java/TelluriumTests.java
@@ -1,8 +1,8 @@
 import com.taucetisoftware.tellurium.Tellurium;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import com.taucetisoftware.tellurium.drivers.DriverFactory;
+import org.junit.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 import static com.taucetisoftware.tellurium.Selector.css;
 
@@ -10,16 +10,37 @@ import static com.taucetisoftware.tellurium.Selector.css;
  * Created by Sergio on 6/2/2016.
  */
 public class TelluriumTests extends Tellurium {
-
+	private static WebDriver tempWebDriver;
 
 	@BeforeClass
-	public void CreateDriver() {
+	public static void createDriver() {
+		tempWebDriver = DriverFactory.createPhantomDriver();
+	}
 
+	@AfterClass
+	public static void teardownDriver() {
+		tempWebDriver.quit();
+	}
+
+	//This is done to get around the static BeforeClass
+	@Before
+	public void assignCorrectDriver() {
+		driver = tempWebDriver;
+		setGlobalWaitTime(5);
 	}
 
 	@Test
-	public void TestClick() {
+	public void testClick() {
+		goToGoogle();
+		click(css("a[href*='about.html']"));
 
+		Assert.assertTrue(getTitle().toLowerCase().contains("about google"));
+	}
+
+	@Test
+	public void testGo() {
+		goToGoogle();
+		Assert.assertTrue(getUrl().contains("google"));
 	}
 
 
@@ -28,5 +49,53 @@ public class TelluriumTests extends Tellurium {
 		click(css("Some CSS"));
 	}
 
+	@Test
+	public void testGetTitle() {
+		goToGoogle();
+		Assert.assertTrue(getTitle().toLowerCase().contains("google"));
+	}
 
+	@Test
+	public void testGetSource() {
+		goToGoogle();
+		Assert.assertTrue(getPageSource().contains("</html>") && getPageSource().contains("</body>"));
+	}
+
+	@Test
+	public void testGoBack() {
+		goBackToGoogle();
+	}
+
+	@Test
+	public void testRefresh() {
+		goToGoogle();
+		refresh();
+		Assert.assertTrue(getUrl().contains("google"));
+	}
+
+	@Test
+	public void testGoForward() {
+		goBackToGoogle();
+		goForward();
+		Assert.assertTrue(getUrl().contains("microsoft"));
+	}
+
+	@Test
+	public void executeJs(){
+		goToGoogle();
+		executeJavascript("window.location.href = 'http://github.com';");
+		sleep(2000);
+		Assert.assertTrue(getUrl().contains("microsoft"));
+	}
+
+	private void goBackToGoogle() {
+		goToGoogle();
+		go("http://www.microsoft.com");
+		goBack();
+		Assert.assertTrue(getUrl().contains("google"));
+	}
+
+	private void goToGoogle() {
+		go("http://www.google.com");
+	}
 }
